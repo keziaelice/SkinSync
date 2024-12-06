d0daf4333dc5abdf58fa61b69e3887edadfb4090
Elice <keziaelice@gmail.com>
2024-12-06 10:00:25 +0700

    Combined EditProfileView

---
+++ SkinSync-Home/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SkinSync-Home
+//
+//  Created by MacBook Pro on 06/12/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
