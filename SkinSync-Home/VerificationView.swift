8ffae24b27dd99f0fba1cd29b6d2367a68adf3e0
Elice <keziaelice@gmail.com>
2024-12-10 13:24:02 +0700

    Combine Login, SignUp, and Verification View

---
+++ SkinSync-Home/VerificationView.swift
@@ -0,0 +1,18 @@
+//
+//  VerificationView.swift
+//  SkinSync-Home
+//
+//  Created by MacBook Pro on 10/12/24.
+//
+
+import SwiftUI
+
+struct VerificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerificationView()
+}
